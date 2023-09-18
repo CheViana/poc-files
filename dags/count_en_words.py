34652240b3e44280e1dbc46059e26f71a96afec9
Jane R <myemail@myemail.com>
2023-09-18 16:40:29 -0400

    initial

---
+++ dags/count_en_words.py
@@ -0,0 +1,87 @@
+
+from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow.decorators import dag, task
+from pendulum import datetime
+from collections import defaultdict
+import json
+import string
+import secrets
+
+FILE_PATH = '/home/jane/Documents/airflow/inputfiles/lorem.txt'
+REF_DICT = '/home/jane/Documents/airflow/textfiles/dict.txt'
+RESULTS_PATH = '/home/jane/Documents/airflow/textfiles/results_{}.txt'
+WORD_LIST_PATH = '/home/jane/Documents/airflow/textfiles/words_list_{}.txt'
+default_args = {"start_date": datetime(2023, 8, 29)}
+
+
+@dag("count_en_words", schedule=None, default_args=default_args, catchup=False)  # description='Count English words in text', 
+def count_en_words():
+
+    def lines_gen(file_path):
+        with open(file_path) as f:
+            for line in f:
+                yield line
+
+    def get_random_str():
+        return ''.join(
+            secrets.choice(string.ascii_lowercase + string.digits) for i in range(8)
+        )
+
+    @task
+    def create_words_list(*args, **kwargs):
+        # print(f"DAG args {args} kwargs {kwargs}")
+        file_path = kwargs['dag_run'].conf.get('filepath')
+        # file_path = FILE_PATH
+        print(file_path)
+        lines = lines_gen(file_path)
+        words_set = set()
+        for line in lines:
+            if not line.strip():
+                continue
+            without_punct = line.translate(str.maketrans('', '', string.punctuation))
+            words = [w.lower() for w in without_punct.split()]
+            words_set |= set(words)
+        
+        random_str = get_random_str()
+        words_list_path = WORD_LIST_PATH.format(random_str)
+        results_path = RESULTS_PATH.format(random_str)
+
+        with open(words_list_path, 'w') as f:
+            json.dump({"words_list": list(words_set), "results_path": results_path}, f)
+
+        return words_list_path
+ 
+    @task
+    def count_en_words(words_list_path):
+        with open(words_list_path, "r") as f:
+            data = json.load(f)
+
+        words_list = data["words_list"]
+        results_path = data["results_path"]
+
+        with open(REF_DICT) as f:
+            dict_words = f.read().split(' ')
+        filtered_words = [w for w in words_list if w in dict_words]
+        print(f"Words count: {len(words_list)}, en words count: {len(filtered_words)}")
+        with open(results_path, 'w') as f:
+            json.dump({"en_words": len(filtered_words)}, f)
+        
+        return results_path
+
+    @task
+    def echo_results(*args, **kwargs):
+        task_instance = kwargs['ti']
+        filename = task_instance.xcom_pull(task_ids='count_en_words')
+        print(f'Reading results: {filename}')
+        with open(filename, 'r') as f:
+            print(f.read())
+
+    sense_the_results = FileSensor(  # can be defered trigger and not occupy the slot
+        task_id='sense_the_results',
+        filepath="{{ task_instance.xcom_pull(task_ids='count_en_words') }}",
+        poke_interval=3
+    )
+
+    count_en_words(create_words_list()) >> sense_the_results >> echo_results()
+
+count_en_words()
---
+++ webapps/input.py
@@ -0,0 +1,70 @@
+from aiohttp import web, ClientSession, BasicAuth
+import datetime
+
+
+AIRFLOW_API = "http://localhost:8080/api/v1"
+AIRFLOW_USER = 'jane'
+AIRFLOW_PASSWORD = 'jane'
+
+
+app = web.Application()
+
+
+async def on_startup(app):
+    app['airflow_session'] = ClientSession()
+
+
+async def on_shutdown(app):
+    try:
+        session = app['airflow_session']
+        await session.close()
+    except KeyError:
+        pass
+
+
+app.on_shutdown.append(on_shutdown)
+app.on_startup.append(on_startup)
+
+
+async def hello(request):
+    return web.Response(text="Hello, world")
+
+
+async def run(request):
+    cmd = request.match_info['cmd']
+    data = await request.json()
+    print(data)
+    try:
+        file_path = data['filepath']
+        email = data['email']
+        print(f"File path {file_path}, email {email}, cmd {cmd}")
+    except (KeyError, TypeError, ValueError) as e:
+        raise web.HTTPBadRequest(
+            text='Missing required values') from e
+
+    dag_data = {
+        "logical_date": datetime.datetime.now(datetime.timezone.utc).isoformat(timespec='seconds'), # + "Z",
+        "conf": {
+            "filepath": file_path,
+            # "email": email
+        },
+    }
+    print(f"DAG data {dag_data}")
+
+    # post event to kafka topic instead
+    async with app['airflow_session'].post(
+        f'{AIRFLOW_API}/dags/{cmd}/dagRuns',
+        json=dag_data,
+        headers={'Content-Type': 'application/json'},
+        auth=BasicAuth(AIRFLOW_USER, AIRFLOW_PASSWORD),
+    ) as resp:
+        status = resp.status
+        response_json = await resp.json()
+
+    return web.Response(text=f"Accepted, dag status {status}, response {response_json} ")
+
+
+app.add_routes([web.post('/run/{cmd}', run)])
+app.add_routes([web.post('/run/{cmd}/', run)])
+app.add_routes([web.get('/', hello)])
+web.run_app(app, port=8081)
